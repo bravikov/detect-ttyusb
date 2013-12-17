f0bd9ed86e07447075d3c324156479ffd2040855
Dmitry Bravikov <bravikov@gmail.com>
2013-12-17 09:43:30 +0600

    Добавил слежение за /dev через inotify.

--- detect-ttyusb.sh
+++ detect-ttyusb.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
 # Скрипт для слежения за подключением/отключением устройств /dev/ttyUSB и /dev/ttyACM.
-# автор: bravikov@gmail.com
-
+#
+# Сайт проекта: https://github.com/bravikov/detect-ttyusb
+#
+# Зависимости:
+#    inotify-tools (для наблюдения за изменениями вкаталоге /dev)
+#    notify-send (для уведомлений на рабочем столе)
+#
+# С помощью утилиты из inotify-tools скрипт устанавливает слежение за каталогом /dev и в случае его изменения дает об этом знать. Если inotify-tools не установлен, то скрипт проверяет каталог /dev раз в секунду.
+#
 # Пример вывода:
 #[01.01.2013 20:20:01] Подключено ttyUSB0
 #[01.01.2013 20:20:05] Подключено ttyUSB1
 #[01.01.2013 20:20:08] Отключено ttyUSB0
 #[01.01.2013 20:20:10] Отключено ttyUSB1
 
-NOTIFY_SEND_SUMMARY="Слежение за устройствами ttyUSB*"
+
+NOTIFY_SEND_SUMMARY="Слежение за устройствами /dev/ttyUSB и /dev/ttyACM"
+
+DIR="/dev"
 
 PREV_DEV_LIST=""
 while [ 1 ]
 do
-    DEV_LIST="`ls /dev | grep 'ttyUSB\|ttyACM'`"
+    DEV_LIST="`ls \"$DIR\" | grep 'ttyUSB\|ttyACM'`"
     
     ##### find new device #####
     for DEV in $DEV_LIST
@@ -64,7 +74,11 @@ do
     
     PREV_DEV_LIST="$DEV_LIST"
     
-    sleep 1
-    
+    if [ -x "`which inotifywait`" ]
+        then
+            inotifywait -e create -e delete "$DIR" > /dev/null 2>&1
+        else
+            sleep 1
+    fi
 done
 
